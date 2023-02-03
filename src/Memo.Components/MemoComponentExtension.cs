9b7d141d5010bea5b3ab4b8fa889b9c9eccb2dc3
token <239573049@qq.com>
2023-02-03 17:58:10 +0800

    项目初始化
    Signed-off-by: token <239573049@qq.com>

---
+++ src/Memo.Components/MemoComponentExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class MemoComponentExtension
+{
+    public static IServiceCollection AddMemoComponent(this IServiceCollection services)
+    {
+        services.AddMasaBlazor();
+        return services;
+    }
+}
